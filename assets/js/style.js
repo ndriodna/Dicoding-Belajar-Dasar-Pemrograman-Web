283ff6987a2f3013aa66476929a4391ba7896aeb
ndriodna <kurodesu6969@gmail.com>
2024-02-23 10:35:03 +0800

    fix: responsive, add: hamburger btn with dropdown using js

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,11 +1,24 @@
-let btnScroll = document.getElementById("btnFloat")
+// menampilkan scroll to top btn ketika scroll mencapai 500px
+window.addEventListener("scroll", () => {
+  let btnScroll = document.getElementById("btnFloat");
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
 
-window.onscroll = function(){scrollToTop()}
+  if (scrollTop > 500) {
+    btnScroll.style.display = "block";
+  } else {
+    btnScroll.style.display = "none";
+  }
+});
 
-function scrollToTop(){
-    if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
-        btnScroll.style.display = "block"
-    } else{
-        btnScroll.style.display = "none"
-    }   
-}
+// menampilkan menu dropdown ketika klik btn hamburger
+let hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  let menu = document.getElementById("menuMobile");
+  if (menu.style.display === "none") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+});
